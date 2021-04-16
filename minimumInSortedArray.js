54333143b65781eba21f39f547a101e0b44fb5a3
KirkNotCaptain <cmariekirk@gmail.com>
2021-04-16 09:50:27 -0400

    min sorted array modified solution

--- minimumInSortedArray.js
+++ minimumInSortedArray.js
@@ -3,34 +3,30 @@
  * @return {number}
  */
  var findMin = function(nums) {
-    
   if (nums.length === 0) return -1;
-  if (nums.length === 1) return nums;
-  
-  if (nums[0] < nums[nums.length - 1]) return nums[0]; //if no rotation return first element
+  if (nums.length === 1) return nums[0];
   
   var left = 0;
   var right = nums.length - 1;
   
+  //if nums is not rotated, return first element
+  if (nums[left] < nums[right]) return nums[0];
+  
   while (right >= left) {
       var mid = Math.floor(left + (right - left) / 2);
       
-      if (nums[mid] > nums[mid+1]) {
-          return nums[mid+1];
-      }
+      if (nums[mid+1] < nums[mid]) return nums[mid + 1];
+      if (nums[mid-1] > nums[mid]) return nums[mid];
       
-      if (nums[mid - 1] > nums[mid]) {
-          return nums[mid]
-      }
-      
-      
-      //if mid element > first element, then keep looking to the right by shifting left
-      if (nums[mid] > nums[0]) {
+      //look right
+      if (nums[right] < nums[mid]) {
           left = mid + 1;
       } else {
-          //if mid element < first element, then keep looking left by shifting right
-          right = mid - 1
+          right = mid - 1;
       }
+      
   }
+  
+  return -1;
   
 };
