d7e1e8445f266b4d2600b19815ef89faf62abbff
KirkNotCaptain <cmariekirk@gmail.com>
2021-04-28 11:38:39 -0400

    Container with Most Water Solution

---
+++ containerWithMostWater.js
@@ -0,0 +1,20 @@
+var maxArea = function (height) {
+	if (height.length === 0 || height.length === 1) return 0;
+
+	let result = 0;
+	let left = 0;
+	let right = height.length - 1;
+
+	while (left < right) {
+		let area = (right - left) * Math.min(height[left], height[right]);
+		result = Math.max(result, area);
+
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+
+	return result;
+};
